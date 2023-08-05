aa4a034cd15123e9401e8b93d3dd0c94f090df90
Austin Johnson <44075033+ajohnson132490@users.noreply.github.com>
2023-08-05 20:57:18 +0000

    working on getting print to work

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@
 int main() {
     Roster r;
     r.add("200215493", "Austin", "Johnson", "ajohnson132490@gmail.com", 21, 2, 3, 4, SOFTWARE);
-    r.printAll();
+    r.add("200215493", "Jeremiah", "Johnson", "ajohnson132490@gmail.com", 21, 2, 3, 4, SOFTWARE);
+
+    //r.printAll();
 }
--- roster.cpp
+++ roster.cpp
@@ -6,19 +6,23 @@ void Roster::add(std::string studentID, std::string firstName, std::string lastN
  int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram) {
     //Get the Days into an array
     int daysInCourse[] = {daysInCourse1, daysInCourse2, daysInCourse3};
-    std::cout<<(int)(sizeof(classRosterArray)/sizeof(classRosterArray[0])) + 1;
+    int size = (int)(sizeof(classRosterArray)/sizeof(classRosterArray[0]));
+    std::cout<<(int)(sizeof(classRosterArray)/sizeof(classRosterArray[0])) + 1<<"\n";
 
     //Create a new student, array, and pointer for array
     Student* tmp = new Student(studentID, firstName, lastName, emailAddress, age, daysInCourse, degreeprogram);
-    Student* newClassRosterArray[(int)(sizeof(classRosterArray)/sizeof(classRosterArray[0])) + 1];
+    Student** newClassRosterArray = new Student*[size+1];
 
-    //Add the new student in at the top of the list
-    newClassRosterArray[0] = tmp;
+
 
     //Transfer over the rest of the students
-    for (int i = 0; i < (int)((int)(sizeof(classRosterArray)/sizeof(classRosterArray[0]))); i++) {
-        newClassRosterArray[i+1] = classRosterArray[i];
+    for (int i = 0; i < (int)((int)(sizeof(classRosterArray)/sizeof(classRosterArray[0]))); ++i) {
+        newClassRosterArray[i] = classRosterArray[i];
+
     }
+
+    //Add the new student in at the top of the list
+    newClassRosterArray[size+1] = tmp;
 
     //Be good to the memory
     classRosterArray = newClassRosterArray;
@@ -30,7 +34,7 @@ void Roster::remove(std::string studentID) {
 
 //Printing Methods
 void Roster::printAll() {
-    for (int i = 0; i < (int)((int)(sizeof(classRosterArray)/sizeof(classRosterArray[0]))); i++) {
+    for (int i = 0; i < (int)((int)(sizeof(classRosterArray)/sizeof(classRosterArray[0]))); ++i) {
         std::cout<<classRosterArray[i]->getFirstName();
     }
 }
